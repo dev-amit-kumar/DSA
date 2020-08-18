5434b1b2d93315b1663753fea3f3a2d67f85428e
Amit Kumar <amitkumar.developer1@gmail.com>
2020-08-18 12:57:22 +0000

    Sync LeetCode submission - Two Sum (python3)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,6 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0,len(nums),1): 
-            for j in range(i+1,len(nums),1):
-                if (nums[i]+nums[j]==target):
-                    return i,j
+        keep = {}
+        for i in range(len(nums)):
+            k = target - nums[i]
+            if k in keep:
+                return [keep[k],i]
+            else:
+                keep[nums[i]] = i
