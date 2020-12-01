e1e0c2c993a496b6cc428d8a4de65f76d4763a59
Amit Kumar <amitkumar.developer1@gmail.com>
2020-12-01 11:11:23 +0000

    Sync LeetCode submission - Next Permutation (python3)

---
+++ my-folder/problems/next_permutation/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = j = len(nums) - 1
+        while i > 0 and nums[i-1] >= nums[i]:
+            i -= 1
+        if i == 0:
+            nums.reverse()
+            return
+        
+        k = i - 1
+        
+        while nums[j] <= nums[k]:
+            j -= 1
+        
+        nums[k], nums[j] = nums[j], nums[k]
+        
+        l = k + 1
+        r = len(nums) - 1
+        
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
