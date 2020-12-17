51681f0ca0637df64356fef6566d7be735fb3c28
Amit <amitkumar.developer1@gmail.com>
2020-12-17 23:07:24 +0530

    Added 1 program

--- learning oops.py
+++ learning oops.py
@@ -1,29 +1,68 @@
-class School:
+class AttainuAU16:
+    attendance = 0
 
     def __init__(self, name, age, rollno, std):
         self.name = name
         self.age = age
         self.rollno = rollno
         self.std = std
-    
+
     def mark_attendance(self):
         print("student ", self.name, "is marking his attenance")
         print("student ", self.name, "marked successfully")
-    
+        self.attendance += 1
+
     def getData(self):
-        print("Name = ",self.name)
-        print("Age = ",self.age)
-        print("Roll no = ",self.rollno)
-        print("Std = ",self.std)
+        print("Name = ", self.name)
+        print("Age = ", self.age)
+        print("Roll no = ", self.rollno)
+        print("Std = ", self.std)
+        print("Total Attendance = ", self.attendance)
+
 
 if __name__ == "__main__":
-    student1 = School("Ayushi Verma", 24, 1, 12)
-    # student2 = School("ABC", 24, 2, 12)
-    # print(student1.name)
-    # student1.mark_attendance()
-    # student1.getData()
-    # fees = 1500
-    # student1.submitFees(fees)
-    # student2.mark_attendance()
-    # objName = className() -> this is a way to make a new object and inside bracket we can send the values of that obj
+    student1 = AttainuAU16("Amit", 24, 1, 12)
 
+    murgan = AttainuAU16("Murgan", 25, 12345, 'MERN STACK')
+    murgan.mark_attendance()
+    murgan.mark_attendance()
+    murgan.mark_attendance()
+    murgan.getData()
+
+    amit = AttainuAU16("Amit", 25, 12345, 'MERN STACK')
+    amit.mark_attendance()
+    amit.mark_attendance()
+    amit.mark_attendance()
+    amit.getData()
+
+    ankit = AttainuAU16("Ankit", 25, 12345, 'MERN STACK')
+    ankit.mark_attendance()
+    ankit.mark_attendance()
+    ankit.mark_attendance()
+    ankit.getData()
+
+
+debbared student(which have less than 3 attendance)
+if conditions -> logic
+
+
+class Students:
+
+
+class insturctor:
+    attendance
+
+
+class mentor:
+    attendance
+    check cc/asg
+    check test
+    check your project
+
+
+class TA:
+    attendance
+    start sesion
+
+
+encapsulation
---
+++ remove duplicate.py
@@ -0,0 +1,5 @@
+# prgram to remve the duplicates
+arr = [1, 2, 3, 1, 2, 3, 4, 5]
+# new_set = (arr)
+new_set = set(arr)
+print(new_set)
