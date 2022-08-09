681a8f10b38df255e4e58a13d0164e997a8606c5
Amit Kumar <amitkumar.developer1@gmail.com>
2022-08-09 05:35:25 +0000

    Sync LeetCode submission - Two Sum (python)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,9 +1,13 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        keep = {}
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashmap = {}
         for i in range(len(nums)):
-            k = target - nums[i]
-            if k in keep:
-                return [keep[k],i]
-            else:
-                keep[nums[i]] = i
+            balance = target - nums[i]
+            if balance in hashmap and hashmap[balance] != i:
+                return [i, hashmap[balance]]
+            hashmap[nums[i]] = i
