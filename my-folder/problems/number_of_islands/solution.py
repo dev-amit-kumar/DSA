29bca599ac27a44f05d57948fa8517f307af7462
Amit Kumar <amitkumar.developer1@gmail.com>
2022-08-29 18:18:13 +0000

    Sync LeetCode submission - Number of Islands (python)

--- my-folder/problems/number_of_islands/solution.py
+++ my-folder/problems/number_of_islands/solution.py
@@ -1,5 +1,5 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+class Solution(object):
+    def numIslands(self, grid):
         def dfs(r, c):
             if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == '0':
                 return 0
@@ -12,3 +12,4 @@ class Solution:
             return 1
         
         return sum(dfs(i, j) for i in range(len(grid)) for j in range(len(grid[0])))
+        
