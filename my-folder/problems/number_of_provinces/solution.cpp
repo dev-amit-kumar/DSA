604a9352dd829c5ddf3c839ae0a9853968c109f7
Amit Kumar <amitkumar.developer1@gmail.com>
2023-06-21 18:25:25 +0000

    Sync LeetCode submission - Number of Provinces (cpp)

---
+++ my-folder/problems/number_of_provinces/solution.cpp
@@ -0,0 +1,58 @@
+class DisjointSet
+{
+public:
+    vector<int> parent, size;
+    DisjointSet(int n)
+    {
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+            size[i] = i;
+        }
+    }
+
+    int findUPar(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        DisjointSet dsu(n);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (isConnected[i][j] == 1)
+                    dsu.unionBySize(i, j);
+
+        int count = 0;
+        for (int i = 0; i < n; i++)
+            if (dsu.parent[i] == i)
+                count++;
+        return count;
+    }
+};
