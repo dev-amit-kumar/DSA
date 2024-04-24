c5b3f1c2472b7529abf93ab30dba7eae204fe5c5
Amit Kumar <amitkumar.developer1@gmail.com>
2024-04-24 21:01:51 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- my-folder/problems/number_of_islands/solution.cpp
+++ my-folder/problems/number_of_islands/solution.cpp
@@ -1,36 +1,27 @@
-class Solution
-{
-private:
-    void dfs(vector<vector<char>> &grid, int rows, int cols, int i, int j)
-    {
-        if (i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j] != '1')
-            return;
-        grid[i][j] = 2;
-        dfs(grid, rows, cols, i, j + 1);
-        dfs(grid, rows, cols, i, j - 1);
-        dfs(grid, rows, cols, i + 1, j);
-        dfs(grid, rows, cols, i - 1, j);
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid, int i, int j, int rows, int cols){
+        if( i < 0 || i == rows || j < 0 || j == cols || grid[i][j] != '1') return;
+        grid[i][j] = '2';
+        dfs(grid, i+1, j, rows, cols);
+        dfs(grid, i-1, j, rows, cols);
+        dfs(grid, i, j+1, rows, cols);
+        dfs(grid, i, j-1, rows, cols);
     }
 
-public:
-    int numIslands(vector<vector<char>> &grid)
-    {
-        int num = 0;
+    int numIslands(vector<vector<char>>& grid) {
+        int nums = 0;
         int rows = grid.size();
-        if (rows == 0)
-            return 0;
+        if(rows == 0) return 0;
         int cols = grid[0].size();
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                if (grid[i][j] == '1')
-                {
-                    num++;
-                    dfs(grid, rows, cols, i, j);
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                if(grid[i][j] == '1'){
+                    nums++;
+                    dfs(grid, i, j, rows, cols);
                 }
             }
         }
-        return num;
+        return nums;
     }
 };
