4e51d84c33dc04d3bbf46ea16432b92202892c7a
Amit Kumar <amitkumar.developer1@gmail.com>
2024-06-15 13:42:27 +0000

    Sync LeetCode submission - Count Negative Numbers in a Sorted Matrix (cpp)

--- my-folder/problems/count_negative_numbers_in_a_sorted_matrix/solution.cpp
+++ my-folder/problems/count_negative_numbers_in_a_sorted_matrix/solution.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int countNegatives(vector<vector<int>>& grid) {
-        int count = 0;
-        int len = grid[0].size();
-        int row_len = grid.size();
-        for (int i = 0; i < row_len; i++)
-        {
-            vector<int> col = grid[i];
-            int left = 0;
-            int right = len - 1;
-            while (left <= right)
-            {
-                int mid = (right + left) / 2;
-                if (col[mid] < 0)
-                    right = mid - 1;
-                else
-                    left = mid + 1;
+        int m = grid.size();
+        int n = grid[0].size();
+        int total = 0;
+        for (int i = 0; i < m; i++){
+            int left = 0, right = n-1;
+            while(left <= right){
+                int mid = (left + right) / 2;
+                if(grid[i][mid] < 0) right = mid - 1;
+                else left = mid + 1;
             }
-            count += (len - left);
+            total += n - left;
         }
-        return count;
+        return total;
     }
 };
