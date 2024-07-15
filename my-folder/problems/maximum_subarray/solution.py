0519b069c1f48b00e49ef1ce8fa0b4b028b8590d
Amit Kumar <amitkumar.developer1@gmail.com>
2024-07-15 13:29:55 +0000

    Sync LeetCode submission - Maximum Subarray (python3)

--- my-folder/problems/maximum_subarray/solution.py
+++ my-folder/problems/maximum_subarray/solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         currSum = 0
-        overAllSum = float('-inf')
-        for i in nums:
-            if(currSum + i > i):
-                currSum += i
+        overall = float('-inf')
+        for num in nums:
+            if(currSum + num > num):
+                currSum += num
             else:
-                currSum = i
-            overAllSum = max(overAllSum, currSum)
-        return overAllSum
+                currSum = num
+            overall = max(overall, currSum)
+        return overall
