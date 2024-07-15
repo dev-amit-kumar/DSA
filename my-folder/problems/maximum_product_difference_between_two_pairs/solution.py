ddcb07c471b1f5e6ea35cea78679485f286a9d62
Amit Kumar <amitkumar.developer1@gmail.com>
2024-07-15 13:23:07 +0000

    Sync LeetCode submission - Maximum Product Difference Between Two Pairs (python3)

--- my-folder/problems/maximum_product_difference_between_two_pairs/solution.py
+++ my-folder/problems/maximum_product_difference_between_two_pairs/solution.py
@@ -1,7 +1,6 @@
 class Solution:
+
     def maxProductDifference(self, nums: List[int]) -> int:
-        # nums.sort()
-        # return (nums[-1] * nums[-2]) - (nums[0] * nums[1])
         smallest, second_smallest = float('inf'), float('inf')
         highest, second_highest = float('-inf'), float('-inf')
         for i in nums:
@@ -10,11 +9,9 @@ class Solution:
                 highest = i
             elif i > second_highest:
                 second_highest = i
-
             if i < smallest:
                 second_smallest = smallest
                 smallest = i
             elif i < second_smallest:
                 second_smallest = i
         return (highest * second_highest) - (smallest * second_smallest)
-
