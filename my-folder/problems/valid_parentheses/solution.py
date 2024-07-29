b67f0a80dc069978c3112376dc18406ef8b7cdf3
Amit Kumar <amitkumar.developer1@gmail.com>
2024-07-29 06:56:26 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

--- my-folder/problems/valid_parentheses/solution.py
+++ my-folder/problems/valid_parentheses/solution.py
@@ -2,12 +2,18 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         for i in s:
-            stack.append(i)
-            if(len(stack) > 1):
-                if (stack[-2] + i) == "()" or (stack[-2] + i) == "{}" or (stack[-2] + i) == "[]":
+            if (i in [")", "}", "]"]):
+                if (len(stack) == 0):
+                    return False
+                if (i == ")" and stack[-1] == "("):
                     stack.pop()
+                elif (i == "}" and stack[-1] == "{"):
                     stack.pop()
-        if(len(stack) == 0):
-            return True
-        else:
-            return False
+                elif (i == "]" and stack[-1] == "["):
+                    stack.pop()
+                else: return False
+            elif(i in ["(", "[", "{"]):
+                stack.append(i)
+            else:
+                return False
+        return True if len(stack) == 0 else False
