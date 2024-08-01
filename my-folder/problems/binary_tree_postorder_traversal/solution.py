764d98dd08f5ccd6b2e7b24da2d1904617ef2e8c
Amit Kumar <amitkumar.developer1@gmail.com>
2024-08-01 18:54:07 +0000

    Sync LeetCode submission - Binary Tree Postorder Traversal (python3)

--- my-folder/problems/binary_tree_postorder_traversal/solution.py
+++ my-folder/problems/binary_tree_postorder_traversal/solution.py
@@ -1,5 +1,17 @@
-class Solution(object):
-    def postorderTraversal(self, root):
-        if root is None: 
-            return []
-        return  self.postorderTraversal(root.left)  + self.postorderTraversal(root.right) + [root.val]
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = [root]
+        result = []
+        while stack:
+            node = stack.pop()
+            if node:
+                result.append(node.val)
+                stack.append(node.left)
+                stack.append(node.right)
+        return result[::-1]
