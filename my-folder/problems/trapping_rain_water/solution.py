71bf344482a47ce5892b7799d88ec6e560316362
Amit Kumar <amitkumar.developer1@gmail.com>
2024-08-01 17:30:27 +0000

    Sync LeetCode submission - Trapping Rain Water (python3)

--- my-folder/problems/trapping_rain_water/solution.py
+++ my-folder/problems/trapping_rain_water/solution.py
@@ -1,22 +1,15 @@
 class Solution:
-    def trap(self, h: List[int]) -> int:
-        suf = []
-        pre = []
-        temp = h[0]
-        res = 0
-        n = len(h) - 1
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        prefix_arr = [0] * n 
+        sufix_arr = [0] * n
+        prefix_arr[0] = height[0]
+        sufix_arr[n-1] = height[n-1]
+        for i in range(1, n):
+            prefix_arr[i] = max(prefix_arr[i-1], height[i])
+        for i in range(n-2, -1, -1):
+            sufix_arr[i] = max(sufix_arr[i+1], height[i])
+        result = 0
         for i in range(0, n):
-            if (h[i] > temp):
-                temp = h[i]
-            pre.append(temp)
-
-        temp = h[-1]
-        for i in range(n, -1, -1):
-            if (h[i] > temp):
-                temp = h[i]
-            suf.append(temp)
-
-        for i in range(0, n):
-            temp = min(pre[i], suf[n-i])
-            res += temp - h[i]
-        return res
+            result += min(prefix_arr[i], sufix_arr[i]) - height[i]
+        return result
