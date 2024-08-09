cd0857e6883598d68c614582047be070ee783a36
Amit Kumar <amitkumar.developer1@gmail.com>
2024-08-09 10:20:09 +0000

    Sync LeetCode submission - Number of Good Pairs (python3)

--- my-folder/problems/number_of_good_pairs/solution.py
+++ my-folder/problems/number_of_good_pairs/solution.py
@@ -1,24 +1,14 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        # Brute force
-        # ans = []
-        # for i in range(0, len(nums)):
-        #     for j in range(i+1, len(nums)):
-        #         if (nums[i] == nums[j]):
-        #             ans.append([i,j])
-        # return len(ans)
-
-        # using hash_map
         hash = {}
         for i in nums:
             if i in hash:
                 hash[i] += 1
             else:
                 hash[i] = 1
+        print(hash)
         total = 0
         for i in nums:
-            # if hash[i] == 1:
-            #     total += 0
             if hash[i] > 1:
                 a = hash[i]
                 val = ((a)*(a-1))/2
