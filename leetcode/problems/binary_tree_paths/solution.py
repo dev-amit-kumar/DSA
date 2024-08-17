666950cf1a2ace6e4b9d7159beb2b15d777acc9c
Amit Kumar <amitkumar.developer1@gmail.com>
2024-08-17 16:20:56 +0000

    Sync LeetCode submission - Binary Tree Paths (python3)

--- leetcode/problems/binary_tree_paths/solution.py
+++ leetcode/problems/binary_tree_paths/solution.py
@@ -1,13 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def binaryTreePaths(self, root: TreeNode) -> List[str]:
-        self.result = []
-        self.find_route(root, '')
-        return self.result
-
-    def find_route(self, root, all_route):
-        if root is None:
-            return None
-        if not root.left and not root.right:
-            self.result.append(all_route + str(root.val))
-        self.find_route(root.left, all_route + str(root.val) + '->')
-        self.find_route(root.right, all_route + str(root.val)+ '->' )
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        def helper(root, currPath):
+            if len(currPath) > 0:
+                currPath += "->" + str(root.val)
+            else:
+                currPath = str(root.val)
+            if root.left is None and root.right is None:
+                ans.append(currPath)
+                return
+            if root.left:
+                helper(root.left, currPath)
+            if root.right:
+                helper(root.right, currPath)
+        ans = []
+        helper(root, '')
+        return ans
