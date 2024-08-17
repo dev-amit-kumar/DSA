da45fb634b487bc8fc0cea4d8436192fd2fd3c97
Amit Kumar <amitkumar.developer1@gmail.com>
2024-08-17 09:31:23 +0000

    Sync LeetCode submission - Fibonacci Number (python3)

--- leetcode/problems/fibonacci_number/solution.py
+++ leetcode/problems/fibonacci_number/solution.py
@@ -1,12 +1,5 @@
 class Solution:
-    def recursion(self, n: int, dp) -> int:
-        if(n <= 1):
-            return n
-        if (n in dp):
-            return dp[n]
-        dp[n] = self.recursion(n-1, dp) + self.recursion(n-2, dp)
-        return dp[n]
-
     def fib(self, n: int) -> int:
-        dp = {}
-        return self.recursion(n, dp)
+        if n < 2:
+            return n
+        return self.fib(n-1) + self.fib(n-2)
