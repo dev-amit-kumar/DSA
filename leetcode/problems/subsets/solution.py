559b8638a78192492a84e190c4dff737ad6613ab
Amit Kumar <amitkumar.developer1@gmail.com>
2024-08-17 09:17:18 +0000

    Sync LeetCode submission - Subsets (python3)

--- leetcode/problems/subsets/solution.py
+++ leetcode/problems/subsets/solution.py
@@ -1,16 +1,12 @@
-from copy import deepcopy
-
-class Solution(object):
+class Solution:
     def subsets(self, nums):
-        total_sets = []
-        def subsetsFn(nums, l, r, total):
-            if l>r:
-                total_sets.append(total)
-                return
-            new_total = deepcopy(total)
-            new_total.append(nums[l])
-            subsetsFn(nums, l+1, r, new_total)
-            subsetsFn(nums, l+1, r, total)
-        subsetsFn(nums, 0, len(nums)-1, [])
-        return total_sets
-        
+        output = [[]]
+        for num in nums:
+            newSubsets = []
+            for curr in output:
+                temp = curr.copy()
+                temp.append(num)
+                newSubsets.append(temp)
+            for curr in newSubsets:
+                output.append(curr)
+        return output
