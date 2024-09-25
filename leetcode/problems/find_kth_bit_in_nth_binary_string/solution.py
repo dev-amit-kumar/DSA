0dd20338a4b4abdef941c477d5d0e162e65d32c2
Amit Kumar <amitkumar.developer1@gmail.com>
2024-09-25 11:09:52 +0000

    Sync LeetCode submission - Find Kth Bit in Nth Binary String (python3)

---
+++ leetcode/problems/find_kth_bit_in_nth_binary_string/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findKthBit(self, n: int, k: int) -> str:
+        def f(n,k):
+            m = floor((2**(n-1) + 0.5))
+            if n == 1 and k == 1:
+                return 0
+            elif n != 1 and k == m:
+                return 1
+            elif k < m:
+                return f(n-1,k)
+            elif k > m:
+                return not f(n,m - abs(m-k))
+        return str(int(f(n,k)))
