ad8e6c5bf4f055bcb9e637a5df3290a17eacb79b
Amit Kumar <amitkumar.developer1@gmail.com>
2024-10-01 15:24:51 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- leetcode/problems/number_of_islands/solution.py
+++ leetcode/problems/number_of_islands/solution.py
@@ -1,15 +1,20 @@
-class Solution(object):
-    def numIslands(self, grid):
-        def dfs(r, c):
-            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == '0':
-                return 0
-            
-            grid[r][c] = '0'
-            
-            for i, j in zip((r - 1, r + 1, r, r), (c, c, c - 1, c + 1)):
-                dfs(i, j)
-            
-            return 1
-        
-        return sum(dfs(i, j) for i in range(len(grid)) for j in range(len(grid[0])))
-        
+class Solution:
+    def dfs(self, i, j, rows, cols, grid):
+        if i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j] != '1':
+            return
+        grid[i][j] = '2'
+        self.dfs(i, j+1, rows, cols, grid)
+        self.dfs(i, j-1, rows, cols, grid)
+        self.dfs(i+1, j, rows, cols, grid)
+        self.dfs(i-1, j, rows, cols, grid)
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        cols = len(grid[0])
+        rows = len(grid)
+        count = 0
+        for i in range(0, rows):
+            for j in range(0, cols):
+                if grid[i][j] == '1':
+                    count += 1
+                    self.dfs(i, j, rows, cols, grid)
+        return count
