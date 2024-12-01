53ef9f97982decb7ae40aa57be4f5b53b4a78cbb
Amit Kumar <amitkumar.developer1@gmail.com>
2024-12-01 17:25:42 +0000

    Sync LeetCode submission - Insert Interval (python3)

--- leetcode/problems/insert_interval/solution.py
+++ leetcode/problems/insert_interval/solution.py
@@ -1,14 +1,19 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        res = []
-        for i in range(len(intervals)):
-            first, second = intervals[i]
-            if first > newInterval[1]:
-                res.append(newInterval)
-                return res + intervals[i:]
-            elif second < newInterval[0]:
-                res.append(intervals[i])
-            else:
-                newInterval = [min(first, newInterval[0]), max(second, newInterval[1])]
-        res.append(newInterval)
-        return res
+        i = 0
+        n = len(intervals)
+
+        # copy the first intervals which are not overlapping and occur before the newInterval
+        while (i < n and intervals[i][1] < newInterval[0]):
+            i += 1
+        
+        beforeOverlap = i
+
+        # create new interval based on the overlapping
+        while(i < n and intervals[i][0] <= newInterval[1]):
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+
+        # return the result
+        return intervals[:beforeOverlap] + [newInterval] + intervals[i:]
